91f789275186d80a895d4ae19795f2170aec1912
LanaKolisnyk <svkolesnik@gmail.com>
2022-11-03 12:47:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ export default function App() {
     <div className="App">
       Weather App 
       <footer>This project was codded by Lana Kolisnyk{" "}
-      <a href="https://heroic-creponne-a9613f.netlify.app/" target="_blank" title="github" rel="noopener noreferrer"></a>
+      <a href="https://heroic-creponne-a9613f.netlify.app/" target="_blank"
+          rel="noopener noreferrer"></a>
     {" "} and is opensourced on GitHub</footer></div>
   );
 }
