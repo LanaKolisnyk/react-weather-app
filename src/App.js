c69b8c251923f0dec9f679de00e4e0379ab97c98
LanaKolisnyk <svkolesnik@gmail.com>
2022-11-03 12:44:04 +0200

    Added link to Github

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-    </div>
+      Weather App 
+      <footer>This project was codded by Lana Kolisnyk{" "}
+      <a href="https://heroic-creponne-a9613f.netlify.app/" target="_blank" title="github" rel="noopener noreferrer"></a>
+    {" "} and is opensourced on GitHub</footer></div>
   );
 }
 
