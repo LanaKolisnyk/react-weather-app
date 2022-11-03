4282decd29a0942ebe1eb5f1f94c42acaf09d73d
LanaKolisnyk <svkolesnik@gmail.com>
2022-11-03 14:49:08 +0200

    Changed link

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ export default function App() {
       Weather App 
       <footer>This project was codded by Lana Kolisnyk{" "}
       
-      <a href="https://heroic-creponne-a9613f.netlify.app/" aria-label="github" target="_blank"
-          rel="noopener noreferrer" alt="github"></a>
-    {" "} and is opensourced on GitHub</footer></div>
+      <a href="https://github.com/LanaKolisnyk/react-weather-app" aria-label="github" target="_blank"
+          rel="noopener noreferrer" alt="github">and is opensourced</a>
+    {" "}  on GitHub</footer></div>
   );
 }
 
