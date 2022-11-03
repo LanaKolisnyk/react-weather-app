6aac655f172d00ee623ca02d6121698005dd3ceb
LanaKolisnyk <svkolesnik@gmail.com>
2022-11-03 14:44:24 +0200

    Added eslintignore

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+
 import React from 'react';
 import './App.css';
 
@@ -6,8 +8,9 @@ export default function App() {
     <div className="App">
       Weather App 
       <footer>This project was codded by Lana Kolisnyk{" "}
-      <a href="https://heroic-creponne-a9613f.netlify.app/" target="_blank"
-          rel="noopener noreferrer"></a>
+      
+      <a href="https://heroic-creponne-a9613f.netlify.app/" aria-label="github" target="_blank"
+          rel="noopener noreferrer" alt="github"></a>
     {" "} and is opensourced on GitHub</footer></div>
   );
 }
