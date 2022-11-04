8550196e646cb5ca4ee94b9d63a18cecda0a9b2a
LanaKolisnyk <svkolesnik@gmail.com>
2022-11-04 19:49:54 +0200

    Added Weather.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 
 import React from 'react';
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App 
+      <div className='container'>
+        <Weather />
+    
       <footer>This project was codded by Lana Kolisnyk{" "}
       
       <a href="https://github.com/LanaKolisnyk/react-weather-app" aria-label="github" target="_blank"
           rel="noopener noreferrer" alt="github">and is opensourced</a>
     {" "}  on GitHub</footer></div>
+    </div>
   );
 }
 
