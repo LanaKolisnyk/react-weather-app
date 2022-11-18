c10a3e9da9e764778850e142dace498b9432e265
LanaKolisnyk <svkolesnik@gmail.com>
2022-11-18 16:19:52 +0200

    Improved WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,28 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("celsius");
-function showFarenheit(event) {
-event.preventDefault();
-setUnit("farenheit");
-}
-
-function showCelsius(event){
-    event.preventDefault();
-    setUnit("celsius");  
-}
-    if (unit === "celsius") {
+    
 return (
     <span>
-     <span className="temperature">{Math.round(props.celsius)}</span><span className="unit"> °C | <a href="/" onClick={showFarenheit}>°F</a></span>
+     <span className="temperature">{Math.round(props.celsius)}</span><span className="unit"> °C </span>
 </span>
 ); 
-} else {
-    let farenheit = (props.celsius * 9/5) + 32;
-    return (
-        <span>
-        <span className="temperature">{Math.round(farenheit)}</span><span className="unit"> <a href="/" onClick={showCelsius}> °C</a> | °F </span>
-   </span>   
-    );
-}
+
 }
